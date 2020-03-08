b5dd3e3fa0c3ec80578af246671d6b44212671e0
Travis Gandy <travis_gandy@yahoo.com>
2020-03-08 21:40:29 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
-def countdown
+def countdown(x = number)
+  number = 10 
+  while number > 0 
+    puts "#{number} SECOND(S)!"
+    number -= 1 
+    sleep(1)
+  end
+  puts "HAPPY NEW YEAR!"
 end
