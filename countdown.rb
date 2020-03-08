b6d642e9e745e6fb71a36d16474cd2948f46dd41
Travis Gandy <travis_gandy@yahoo.com>
2020-03-08 21:50:33 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,9 +2,11 @@
 
 def countdown
   number = 10 
+  countdown_with_sleep = sleep(1)
   while number > 0 
     puts "#{number} SECOND(S)!"
     number -= 1 
+    countdown_with_sleep
   end
   puts "HAPPY NEW YEAR!"
 end
